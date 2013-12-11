ee7583289ec9bed1428e47f8e939831f0387032e
Forrest Cao <forrest@factual.com>
2013-12-10 18:15:27 -0800

    releasing 1.3.11

--- lib/factual/api.rb
+++ lib/factual/api.rb
@@ -1,6 +1,6 @@
 class Factual
   class API
-    VERSION = "1.3.10"
+    VERSION = "1.3.11"
     API_V3_HOST = "api.v3.factual.com"
     DRIVER_VERSION_TAG = "factual-ruby-driver-v" + VERSION
     PARAM_ALIASES = { :search => :q, :sort_asc => :sort }
